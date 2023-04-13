8fc1ca86186157dd34384dc534e8e228797ec9b2
unaschneck <24469269+unaschneck@users.noreply.github.com>
2023-04-13 10:15:14 -0400

    Update ht_diff_comp.m

--- FetchLaws/ht_diff_comp.m
+++ FetchLaws/ht_diff_comp.m
@@ -5,36 +5,38 @@ close all
 % make plots of signifigant wave height for different compositions
 
 
-windspeeds = 0.4:1:3.3;
+windspeeds = [0.4 1];
+
+m = 31;                                                                    % number of gridpoints in x-direction
+n = 15;                                                                    % number of gridpoints in y-direction
+
 bathy_map = 100.*ones(m,n);
 
 % from titanpool at 90K
 
 rho_methane = 540;
-nu_methane = NaN;
+nu_methane = 3e-7; % m2/s
 
-rho_ethane = 660;
-nu_ethane = NaN;
+rho_ethane = 660; % Titanpool and Hayes 2012
+nu_ethane = (0.0011)/rho_ethane; % kinematic viscocity (titanpool Hayes 2012)
 
-% Ontario composition of 51:38:11 percent methane:ethane:nitrogen from Mastrogiuseppe 2018
-rho_ontario = NaN;
-nu_ontario = NaN;
-
-% Punga composition of 80:0:20 percent methane:ethane:nitrogen from Mastrogiuseppe 2018
-rho_punga = NaN;
-nu_punga = NaN;
+% % Ontario composition of 51:38:11 percent methane:ethane:nitrogen from Mastrogiuseppe 2018
+% rho_ontario = NaN;
+% nu_ontario = NaN;
+% 
+% % Punga composition of 80:0:20 percent methane:ethane:nitrogen from Mastrogiuseppe 2018
+% rho_punga = NaN;
+% nu_punga = NaN;
 
 sigH_methane = makeWaves(windspeeds,rho_methane,nu_methane,bathy_map);
 sigH_ethane = makeWaves(windspeeds,rho_ethane,nu_ethane,bathy_map);
-sigH_ontario = makeWaves(windspeeds,rho_ontario,nu_ontario,bathy_map);
-sigH_punga = makeWaves(windspeeds,rho_punga,nu_punga,bathy_map);
+% sigH_ontario = makeWaves(windspeeds,rho_ontario,nu_ontario,bathy_map);
+% sigH_punga = makeWaves(windspeeds,rho_punga,nu_punga,bathy_map);
 
 figure
-plot(u,sigH_methane,'-o')
+plot(windspeeds,sigH_methane(:,2),'-o')
 hold on
-plot(u,sigH_ethane,'-o')
-plot(u,sigH_ontario,'-o')
-plot(u,sigH_punga,'-o')
-legend('Methane-N2','Ethane-N2','Methane-Ethane-N2 (Ontario)','Methane-Ethane-N2 (Punga)')
+plot(windspeeds,sigH_ethane(:,2),'-o')
+legend('Methane-N2','Ethane-N2')
 xlabel('wind speed [m/s]')
 ylabel('sig H [cm]')
