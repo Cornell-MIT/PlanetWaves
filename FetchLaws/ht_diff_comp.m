9cdf00fa81832bf229234a5d9af2f7edfbb852d0
cedetelich13 <charlene.detelich@gmail.com>
2023-05-24 13:27:10 -0400

    ADDED COMPOSITIONS

--- FetchLaws/ht_diff_comp.m
+++ FetchLaws/ht_diff_comp.m
@@ -15,8 +15,8 @@ planet_press = 1.5*101300;
 bathy_map = 100.*ones(m,n);
 gridX = m;
 gridY = n;
-time_step_size = 100; 
-num_time_steps = 10; %usually is 200
+time_step_size = 1; 
+num_time_steps = 100; %usually is 200
 
 %% from titanpool at 90K (220 min for 2 windspeeds; 440 min for 4 windspeeds)
 
@@ -29,93 +29,155 @@ num_time_steps = 10; %usually is 200
 % %% methane:ethane:nitrogen from titanpool at 92K (steckloff et al., 2020) and
 % %NIST
 % 
-% % 0:96:4.27
-% rho_0_96_4 = 652.735;
-% sfct_0_96_4 = 1.209E-3;
-% nu_0_96_4 = 1.209E-3/rho_0_96_4;
-% sigH_0_96_4 = makeWaves(makeWaves(windspeeds,wind_dir,rho_0_96_4,nu_0_96_4, ...
-%     planet_gravity,planet_temp,planet_press,sfct_0_96_4,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 10:86:4.96
-% rho_10_86_4 = 641.081;
-% sfct_10_86_4 = 3.562E-2;
-% nu_10_86_4 = 1.106E-3/rho_10_86_4;
-% sigH_10_86_4 = makeWaves(makeWaves(windspeeds,wind_dir,rho_10_86_4,nu_10_86_4, ...
-%     planet_gravity,planet_temp,planet_press,sfct_10_86_4,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 19:76:5.53
-% rho_19_76_5 = 626.643;
-% sfct_19_76_5 = 3.353E-2;
-% nu_19_76_5 = 1.004E-3/rho_19_76_5;
-% sigH_19_76_5 = makeWaves(makeWaves(windspeeds,wind_dir,rho_19_76_5,nu_19_76_5, ...
-%     planet_gravity,planet_temp,planet_press,sfct_19_76_5,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 28:66:6.35
-% rho_28_66_6 = 612.61;
-% sfct_28_66_6 = 3.144E-2;
-% nu_28_66_6 = 9.02E-4/rho_28_66_6;
-% sigH_28_66_6 = makeWaves(makeWaves(windspeeds,wind_dir,rho_28_66_6,nu_28_66_6, ...
-%     planet_gravity,planet_temp,planet_press,sfct_28_66_6,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 37:55:7.61
-% rho_37_55_8 = 597.556;
-% sfct_37_55_8 = 2.935E-2;
-% nu_37_55_8 = 7.998E-4/rho_37_55_8;
-% sigH_37_55_8 = makeWaves(makeWaves(windspeeds,wind_dir,rho_37_55_8,nu_37_55_8, ...
-%     planet_gravity,planet_temp,planet_press,sfct_37_55_8,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 45:45:9.42
-% rho_45_45_10 = 582.749;
-% sfct_45_45_10 = 2.726E-2;
-% nu_45_45_10 = 6.976E-4/rho_45_45_10;
-% sigH_45_45_10 = makeWaves(makeWaves(windspeeds,wind_dir,rho_45_45_10,nu_45_45_10, ...
-%     planet_gravity,planet_temp,planet_press,sfct_45_45_10,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 53:35:11.52
-% rho_53_35_12 = 567.498;
-% sfct_53_35_12 = 2.517E-2;
-% nu_53_35_12 = 5.954E-4/rho_53_35_12;
-% sigH_53_35_12 = makeWaves(makeWaves(windspeeds,wind_dir,rho_53_35_12,nu_53_35_12, ...
-%     planet_gravity,planet_temp,planet_press,sfct_53_35_12,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 60:26:14.27
-% rho_60_26_14 = 552.24;
-% sfct_60_26_14 = 2.308E-2;
-% nu_60_26_14 = 4.932E-4/rho_60_26_14;
-% sigH_60_26_14 = makeWaves(makeWaves(windspeeds,wind_dir,rho_60_26_14,nu_60_26_14, ...
-%     planet_gravity,planet_temp,planet_press,sfct_60_26_14,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 66:16:17.70
-% rho_66_16_18 = 538.596;
-% sfct_66_16_18 = 2.098E-2;
-% nu_66_16_18 = 3.910E-4/rho_66_16_17;
-% sigH_66_16_18 = makeWaves(makeWaves(windspeeds,wind_dir,rho_66_16_18,nu_66_16_18, ...
-%     planet_gravity,planet_temp,planet_press,sfct_66_16_18,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 71:8:21.59
-% rho_71_8_21 = 524.596;
-% sfct_71_8_21 = 1.889E-2;
-% nu_71_8_21 = 1.106E-3/rho_71_8_21;
-% sigH_71_8_21 = makeWaves(makeWaves(windspeeds,wind_dir,rho_71_8_21,nu_71_8_21, ...
-%     planet_gravity,planet_temp,planet_press,sfct_71_8_21,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
-% 
-% % 75:0:25.24
-% rho_75_0_25 = 512.577;
-% sfct_75_0_25 = 1.68E-2;
-% nu_75_0_25 = 1.867E-4/rho_75_0_25;
-% sigH_75_0_25 = makeWaves(makeWaves(windspeeds,wind_dir,rho_75_0_25,nu_75_0_25, ...
-%     planet_gravity,planet_temp,planet_press,sfct_75_0_25,bathy_map, ...
-%     gridX,gridY,time_step_size,num_time_steps);
+% 0:96:4.27
+rho_0_96_4 = 652.735;
+sfct_0_96_4 = 1.209E-3;
+nu_0_96_4 = 1.209E-3/rho_0_96_4;
+sigH_0_96_4 = makeWaves(windspeeds,wind_dir,rho_0_96_4,nu_0_96_4, ...
+    planet_gravity,planet_temp,planet_press,sfct_0_96_4,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+figure
+plot(1:length(sigH_0_96_4),sigH_0_96_4(3,:), 'Color', [66/255 0/255 83/255] );
+xlabel('# time steps (delt = .1 s)')
+ylabel('sig H [m]')
+title('timestep vs sig H at 92K for 0:96:4.27 (methane:ethane:nitrogen)')
+saveas(gcf,'0_96_4_timevSigH.png')
+figure
+plot(windspeeds,sigH_0_96_4(:,length(sigH_0_96_4)), 'Color', [62/255 66/255 137/255]);
+xlabel('windspeed [m/s]')
+ylabel('sig H [m]')
+title('windspeed vs sig H at 92K for 0:96:4.27 (methane:ethane:nitrogen)')
+saveas(gcf,'0_96_4_windvSigH.png')
+
+% 10:86:4.96
+rho_10_86_4 = 641.081;
+sfct_10_86_4 = 3.562E-2;
+nu_10_86_4 = 1.106E-3/rho_10_86_4;
+sigH_10_86_4 = makeWaves(windspeeds,wind_dir,rho_10_86_4,nu_10_86_4, ...
+    planet_gravity,planet_temp,planet_press,sfct_10_86_4,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+figure
+plot(1:length(sigH_10_86_4),sigH_10_86_4(3,:), 'Color', [66/255 0/255 83/255] );
+xlabel('# time steps (delt = .1 s)')
+ylabel('sig H [m]')
+title('timestep vs sig H at 92K for 10:86:4.96 (methane:ethane:nitrogen)')
+saveas(gcf,'0_96_4_timevSigH.png')
+figure
+plot(windspeeds,sigH_10_86_4(:,length(sigH_10_86_4)), 'Color', [62/255 66/255 137/255]);
+xlabel('windspeed [m/s]')
+ylabel('sig H [m]')
+title('windspeed vs sig H at 92K for 10:86:4.96 (methane:ethane:nitrogen)')
+saveas(gcf,'10_86_4_windvSigH.png')
+
+% 19:76:5.53
+rho_19_76_5 = 626.643;
+sfct_19_76_5 = 3.353E-2;
+nu_19_76_5 = 1.004E-3/rho_19_76_5;
+sigH_19_76_5 = makeWaves(windspeeds,wind_dir,rho_19_76_5,nu_19_76_5, ...
+    planet_gravity,planet_temp,planet_press,sfct_19_76_5,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+figure
+plot(1:length(sigH_19_76_5),sigH_19_76_5(3,:), 'Color', [66/255 0/255 83/255] );
+xlabel('# time steps (delt = .1 s)')
+ylabel('sig H [m]')
+title('timestep vs sig H at 92K for 19:76:5.53 (methane:ethane:nitrogen)')
+saveas(gcf,'19_76_5_timevSigH.png')
+figure
+plot(windspeeds,sigH_19_76_5(:,length(sigH_19_76_5)), 'Color', [62/255 66/255 137/255]);
+xlabel('windspeed [m/s]')
+ylabel('sig H [m]')
+title('windspeed vs sig H at 92K for 19:76:5.53 (methane:ethane:nitrogen)')
+saveas(gcf,'19_76_5_windvSigH.png')
+
+
+% 28:66:6.35
+rho_28_66_6 = 612.61;
+sfct_28_66_6 = 3.144E-2;
+nu_28_66_6 = 9.02E-4/rho_28_66_6;
+sigH_28_66_6 = makeWaves(windspeeds,wind_dir,rho_28_66_6,nu_28_66_6, ...
+    planet_gravity,planet_temp,planet_press,sfct_28_66_6,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+figure
+plot(1:length(sigH_28_66_6),sigH_28_66_6(3,:), 'Color', [66/255 0/255 83/255] );
+xlabel('# time steps (delt = .1 s)')
+ylabel('sig H [m]')
+title('timestep vs sig H at 92K for 28:66:6.35 (methane:ethane:nitrogen)')
+saveas(gcf,'28_66_6_timevSigH.png')
+figure
+plot(windspeeds,sigH_28_66_6(:,length(sigH_28_66_6)), 'Color', [62/255 66/255 137/255]);
+xlabel('windspeed [m/s]')
+ylabel('sig H [m]')
+title('windspeed vs sig H at 92K for 28:66:6.35 (methane:ethane:nitrogen)')
+saveas(gcf,'28_66_6_windvSigH.png')
+
+% 37:55:7.61
+rho_37_55_8 = 597.556;
+sfct_37_55_8 = 2.935E-2;
+nu_37_55_8 = 7.998E-4/rho_37_55_8;
+sigH_37_55_8 = makeWaves(windspeeds,wind_dir,rho_37_55_8,nu_37_55_8, ...
+    planet_gravity,planet_temp,planet_press,sfct_37_55_8,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+figure
+plot(1:length(sigH_37_55_8),sigH_37_55_8(3,:), 'Color', [66/255 0/255 83/255] );
+xlabel('# time steps (delt = .1 s)')
+ylabel('sig H [m]')
+title('timestep vs sig H at 92K for 37:55:7.61 (methane:ethane:nitrogen)')
+saveas(gcf,'37_55_8_timevSigH.png')
+figure
+plot(windspeeds,sigH_37_55_8(:,length(sigH_37_55_8)), 'Color', [62/255 66/255 137/255]);
+xlabel('windspeed [m/s]')
+ylabel('sig H [m]')
+title('windspeed vs sig H at 92K for 37:55:7.61 (methane:ethane:nitrogen)')
+saveas(gcf,'37_55_8_windvSigH.png')
+
+%% REPLACE THE VALUES WITH THE PLOTS!!!!!
+% 45:45:9.42
+rho_45_45_10 = 582.749;
+sfct_45_45_10 = 2.726E-2;
+nu_45_45_10 = 6.976E-4/rho_45_45_10;
+sigH_45_45_10 = makeWaves(windspeeds,wind_dir,rho_45_45_10,nu_45_45_10, ...
+    planet_gravity,planet_temp,planet_press,sfct_45_45_10,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+
+% 53:35:11.52
+rho_53_35_12 = 567.498;
+sfct_53_35_12 = 2.517E-2;
+nu_53_35_12 = 5.954E-4/rho_53_35_12;
+sigH_53_35_12 = makeWaves(windspeeds,wind_dir,rho_53_35_12,nu_53_35_12, ...
+    planet_gravity,planet_temp,planet_press,sfct_53_35_12,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+
+% 60:26:14.27
+rho_60_26_14 = 552.24;
+sfct_60_26_14 = 2.308E-2;
+nu_60_26_14 = 4.932E-4/rho_60_26_14;
+sigH_60_26_14 = makeWaves(windspeeds,wind_dir,rho_60_26_14,nu_60_26_14, ...
+    planet_gravity,planet_temp,planet_press,sfct_60_26_14,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+
+% 66:16:17.70
+rho_66_16_18 = 538.596;
+sfct_66_16_18 = 2.098E-2;
+nu_66_16_18 = 3.910E-4/rho_66_16_17;
+sigH_66_16_18 = makeWaves(windspeeds,wind_dir,rho_66_16_18,nu_66_16_18, ...
+    planet_gravity,planet_temp,planet_press,sfct_66_16_18,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+
+% 71:8:21.59
+rho_71_8_21 = 524.596;
+sfct_71_8_21 = 1.889E-2;
+nu_71_8_21 = 1.106E-3/rho_71_8_21;
+sigH_71_8_21 = makeWaves(windspeeds,wind_dir,rho_71_8_21,nu_71_8_21, ...
+    planet_gravity,planet_temp,planet_press,sfct_71_8_21,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
+
+% 75:0:25.24
+rho_75_0_25 = 512.577;
+sfct_75_0_25 = 1.68E-2;
+nu_75_0_25 = 1.867E-4/rho_75_0_25;
+sigH_75_0_25 = makeWaves(windspeeds,wind_dir,rho_75_0_25,nu_75_0_25, ...
+    planet_gravity,planet_temp,planet_press,sfct_75_0_25,bathy_map, ...
+    gridX,gridY,time_step_size,num_time_steps);
 
 %% lake compositions from NIST and sources (80 min for 2 windspeeds; 160 min for 4 windspeeds)
 % % Ontario composition of 51:38:11 percent methane:ethane:nitrogen from Mastrogiuseppe 2018
@@ -124,8 +186,7 @@ sfct_ontario = 2.579E-2;
 nu_ontario = 6.261E-4/rho_ontario;
 sigH_ontario = makeWaves(windspeeds,wind_dir,rho_ontario,nu_ontario, ...
     planet_gravity,planet_temp,planet_press,sfct_ontario,bathy_map, ...
-    gridX,gridY,time_step_size,num_time_steps);
-%% 
+    gridX,gridY,time_step_size,num_time_steps); 
 figure
 plot(1:length(sigH_ontario),sigH_ontario(3,:), 'Color', [66/255 0/255 83/255] );
 xlabel('# time steps (delt = .1 s)')
@@ -187,9 +248,7 @@ sfct_ligiamin = 2.6E-2;
 nu_ligiamin = 6.363E-4/rho_ligiamin;
 sigH_ligiamin = makeWaves(windspeeds,wind_dir,rho_ligiamin,nu_ligiamin, ...
     planet_gravity,planet_temp,planet_press,sfct_ligiamin,bathy_map, ...
-    gridX,gridY,time_step_size,num_time_steps);
-figure
-%% 
+    gridX,gridY,time_step_size,num_time_steps); 
 figure
 plot(1:length(sigH_ligiamin),sigH_ligiamin(3,:), 'Color', [66/255 192/255 114/255]);
 xlabel('# time steps (delt = .1 s)')
@@ -216,8 +275,7 @@ plot(1:length(sigH_kraken),sigH_kraken(3,:), 'Color', [253/255 232/255 33/255]);
 xlabel('# time steps (delt = .1 s)')
 ylabel('sig H [m]')
 title('timestep vs sig H at 92K for Kraken')
-saveas(gcf,'Kraken_timevSigH.png')
-%% 
+saveas(gcf,'Kraken_timevSigH.png') 
 figure
 plot(windspeeds,sigH_kraken(:,length(sigH_kraken)), 'Color', [62/255 66/255 137/255]);
 xlabel('windspeed [m/s]')
