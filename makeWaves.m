741c9e0a3bf0d5eeeea5beb6478e0d235a483cc6
unaschneck <24469269+unaschneck@users.noreply.github.com>
2024-02-01 11:20:40 -0500

    added input parameters to logfile

--- makeWaves.m
+++ makeWaves.m
@@ -98,6 +98,82 @@ disp('================================================================')
 disp(['Directional Wave Spectrum -- last updated: ' dir('makeWaves.m').date])
 disp(['Wind Speed(s) to Run: ' regexprep(mat2str(wind.speed),{'\[', '\]', '\s+'}, {'', '', ','}) ' m/s']);
 disp('================================================================')
+
+% -- save input directory to log -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+fprintf('Planet:\t\t\t\t %s\n',planet.name)
+fprintf('\trho_liquid:\t\t %.3f\n',planet.rho_liquid)
+fprintf('\tnu_liquid:\t\t %.3e\n',planet.nu_liquid)
+fprintf('\tnua:\t\t\t %.3e\n',planet.nua)
+fprintf('\tgravity:\t\t %.3f\n',planet.gravity)
+fprintf('\tsurface_temp:\t\t %.3f\n',planet.surface_temp)
+fprintf('\tsurface_press:\t\t %.3f\n',planet.surface_press)
+fprintf('\tsurface_tension:\t %.3f\n',planet.surface_tension)
+
+fprintf('\n')
+
+fprintf('Model\n')
+fprintf('\tm:\t\t\t %i\n',model.m)
+fprintf('\tn:\t\t\t %i\n',model.n)
+fprintf('\to:\t\t\t %i\n',model.o)
+fprintf('\tp:\t\t\t %i\n',model.p)
+fprintf('\tlong:\t\t\t %i\n',model.long)
+fprintf('\tlat:\t\t\t %i\n',model.lat)
+fprintf('\tgridX:\t\t\t %i\n',model.gridX)
+fprintf('\tgridY:\t\t\t %i\n',model.gridY)
+fprintf('\tmindelt:\t\t %.2e\n',model.mindelt)
+fprintf('\tmaxdelt:\t\t %.2f\n',model.maxdelt)
+fprintf('\ttime_step:\t\t %i\n',model.time_step)
+fprintf('\tnum_time_steps:\t\t %i\n',model.num_time_steps)
+fprintf('\ttolH:\t\t\t %.2f\n',model.tolH)
+fprintf('\tcutoff_freq:\t\t %i\n',model.cutoff_freq)
+fprintf('\tmin_freq:\t\t %.2f\n',model.min_freq)
+fprintf('\tmax_freq:\t\t %.2f\n',model.max_freq)
+fprintf('\tz_data:\t\t\t %.2f\n',model.z_data)
+fprintf('\ttune_A1:\t\t %.2f\n',model.tune_A1)
+fprintf('\ttune_mss_fac:\t\t %.2f\n',model.tune_mss_fac)
+fprintf('\ttune_Sdt_fac:\t\t %.3f\n',model.tune_Sdt_fac)
+fprintf('\ttune_Sbf_fac:\t\t %.3f\n',model.tune_Sbf_fac)
+fprintf('\ttune_cotharg:\t\t %.2f\n',model.tune_cotharg)
+fprintf('\ttune_n:\t\t\t %.2f\n',model.tune_n)
+fprintf('\tbathy_map (deepest):\t %.2f\n',max(max(model.bathy_map)))
+
+allSame = all(all(model.bathy_map == model.bathy_map(1,1)) == 1);
+if allSame
+    slopex = 0;
+    slopey = 0;
+else
+    slopex = max(max(model.bathy_map))/(model.m*model.gridX);
+    slopey = max(max(model.bathy_map))/(model.n*model.gridY);
+end
+
+
+fprintf('\tbathy_map (slopex):\t %.2e\n',slopex)
+fprintf('\tbathy_map (slopey):\t %.2e\n',slopey)
+
+
+fprintf('\n')
+
+fprintf('Wind\n')
+[fprintf('\tspeed: \t\t\t'),fprintf(' %.2f,', wind.speed(1:end-1)), fprintf(' %.2f\n', wind.speed(end))];
+fprintf('\tdir: \t\t\t %.2f',wind.dir)
+
+fprintf('\n')
+
+fprintf('Uniflow\n')
+fprintf('\tEast:\t\t\t %.2f\n',uniflow.East)
+fprintf('\tNorth:\t\t\t %.2f\n',uniflow.North)
+
+fprintf('\n')
+
+fprintf('Etc\n')
+fprintf('\tshowplots:\t\t %i\n',Etc.showplots)
+fprintf('\tsavedata:\t\t %i\n',Etc.savedata)
+fprintf('\tshowlog:\t\t %i\n',Etc.showlog)
+
+disp('================================================================')
+
+
 % -- prepare .mat files to be saved during loops -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 file = 1;
 % -- MODEL SET UP ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -194,7 +270,8 @@ ann(:,:,:) = ones(model.m,model.n,model.o);
 m = model.m; n = model.n; g = planet.gravity;
 sfcT = planet.surface_tension; rhow = planet.rho_liquid; mynnn = model.tune_n;                                                % defined here so no broadcast variable in the parfor loop
 
-parfor (jm = 1:m,parcluster)
+
+for jm = 1:m
    for jn = 1:n
        if D(jm,jn) > 0
            wn(jm,jn,:) = wavekgt(f,D(jm,jn),g,sfcT,rhow,1e-4);                                                                 % wave number (using linear wave dispersion)
@@ -204,7 +281,6 @@ parfor (jm = 1:m,parcluster)
    end
 end
 
-delete(gcp('nocreate'));
 % reshape the matrix (dimensions of [n m o p])
 wn = repmat(wn,[1 1 1 model.p]);
 nnn = repmat(nnn,[1 1 1 model.p]);
@@ -325,10 +401,11 @@ for iii=1:numel(wind.speed)                                                % loo
            
            p = model.p;                                                                                                                              % defined locally here so not a broadcast variable in the parfor loop
            
-           parfor (tj = 1:p,parcluster)
+           
+           for tj = 1:p
                short(:,:,:,tj) = sum(E.*cth2(:,:,:,rem((1:p)-tj+p,p)+1),4)*dth;                                                                      % energy in each angular bin get the mean square slope (eqn. 16, Donelan 2012)
            end
-           delete(gcp('nocreate'));
+           
            short = (cumsum((wn.^3.*short.*dwn),3)-wn.^3.*short.*dwn);                                                                                % sqrt of mean square slope (eqn. 16, Donelan 2012)
           
 % -- Sdt ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
